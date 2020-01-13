167c4f7f37cf27b0ebde8d6a261abf4b4620beed
Tetyana <tsergata@gmail.com>
2020-01-13 22:22:30 +0100

    Esercizio fatto

--- pari_dispari/js/script.js
+++ pari_dispari/js/script.js
@@ -18,15 +18,12 @@ while (evenOdd !== 'pari' && evenOdd !== 'dispari') {
 var number = parseInt(prompt('Scegli un numero da 1 a 5'));
   console.log('Il numero inserito :' + ' ' + number);
 
-// Controllo sull'inserimento corretto
-// var numberUser = [1, 2, 3, 4, 5, 6];
-// for (var i = 0; i < numberUser.length; i++) {
-//   console.log('Il numero inserito :' + ' ' + number);
-// }
-// while ( number !==  numberUser[i]) {
-//   number = parseInt(prompt('Non hai inserito un numero tra 1 e 5'));
-// }
 
+// Controllo sull'inserimento corretto
+while (number < 1 || number > 5) {
+  console.log('Il numero non è corretto');
+  number = parseInt(prompt('Scegli un numero da 1 a 5'));
+}
 
 // 2.Generiamo un numero random (sempre da 1 a 5) per il computer.
 var pcNumber = Math.floor(Math.random() * 5) + 1;
@@ -45,5 +42,4 @@ if (sum % 2 == 0 ) {
   result = 'Hai perso, ritenta';
   alert('Hai perso, ritenta');
 }
-
 console.log(result);
